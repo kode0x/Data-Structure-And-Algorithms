6872f25acee78c12f3e9c047808a6a1dfc0d7d3c
kode0x <jyotiradityaxsinghchauhan@gmail.com>
2026-02-11 09:33:55 +0530

    Algorithms

--- Algorithms/Sorting/BubbleSort.cpp
+++ Algorithms/Sorting/BubbleSort.cpp
@@ -1,10 +1,14 @@
-#include<bits/stdc++.h>
-#include<algorithm>
+#include <bits/stdc++.h>
+#include <algorithm>
 
-void bubbleSort(int array[], int size) {
-    for(int one = 0; one < size - 1; one++) {
-        for (int two = 0; two < size - one - 1; two++) {
-            if(array[two] > array[two + 1]) {
+void bubbleSort(int array[], int size)
+{
+    for (int one = 0; one < size - 1; one++)
+    {
+        for (int two = 0; two < size - one - 1; two++)
+        {
+            if (array[two] > array[two + 1])
+            {
                 std::swap(array[two], array[two + 1]);
             }
         }
